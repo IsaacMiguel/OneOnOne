6eb51a419e43d6d056f3ae9a2e0617dd122e7aa6
isaacmiguel <isaacmiguel@gmail.com>
2024-04-29 09:19:22 -0300

    feat(layout): create and style layout
    create a new layout component

--- src/ui/layout/layout.jsx
+++ src/ui/layout/layout.jsx
@@ -1,6 +1,11 @@
 import React from "react"
 
 import { Header, } from "./Header.jsx"
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: grid;
+`
 
 /**
  * @typedef Layout
@@ -9,12 +14,12 @@ import { Header, } from "./Header.jsx"
  */
 export const Layout = ({ children, },) => {
   return (
-    <>
+    <Container>
       <Header title={"Header Title"} />
-      <section role="main">{children}</section>
+      <main role="main">{children}</main>
       <footer>
         <h1>footer</h1>
       </footer>
-    </>
+    </Container>
   )
 }
